cfa713ac40d5b3a1ca94fec4ec5df5f5dc7a0893
woifal <wolfgang.dietersdorfer@gmail.com>
2015-02-24 09:17:47 +0100

    Issue #190: Switch order of menu items..

--- js/_web/webClLib.js
+++ js/_web/webClLib.js
@@ -207,8 +207,8 @@ document.addEventListener("DOMContentLoaded", function() {
 				$('<div style="clear: both"></div>' + 
 					'<ul class="nav menu clMenuContainer">' + 
 						'<li class="current" style="float: left; width: 50%;"><a class="clUserInfo" href="#"></a></li>' + 
-						'<li class="current loggedInOnly hidden clMenuMyRoutes"><a href="http://www.kurt-climbing.com/Joomla/index.php/cllib-stats"><span>My Routes</span></a></li>' + 
 						'<li class="current loggedInOnly hidden clMenuMyStats"><a href="http://www.kurt-climbing.com/Joomla/index.php/cllib-graph"><span>My Statistics</span></a></li>' + 
+						'<li class="current loggedInOnly hidden clMenuMyRoutes"><a href="http://www.kurt-climbing.com/Joomla/index.php/cllib-stats"><span>My Routes</span></a></li>' + 
 	//					'<li class="current"><a href="#">Grade Conversion</a></li>' + 
 	//					'<li class="current"><a href="#">Buddies</a></li>' + 
 	//					'<li class="current"><a id="logoutButton" href="#">Logout</a></li>' + 
