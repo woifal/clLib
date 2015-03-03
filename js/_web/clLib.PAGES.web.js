2a0f20466bc14b5d9aae0eb0933afedd1dc9b126
woifal <wolfgang.dietersdorfer@gmail.com>
2015-03-04 00:01:11 +0100

    Issue #192: move joomla root..

--- js/_web/clLib.PAGES.web.js
+++ js/_web/clLib.PAGES.web.js
@@ -5,12 +5,11 @@ clLib.PAGES.processAuthObj = function(urlAuthObj, successFunc) {
 	console.log("OVERRIDDEN PROCESSAUTHOBJ!!!!" + parent.document.location.href);
 //	if(!successFunc) {
 		successFunc = function() {
-			if(parent.document.location.href == 'http://www.kurt-climbing.com/Joomla/index.php/cllib-users-cllogin') {
+			if(parent.document.location.href == 'http://www.kurt-climbing.com/KURT/html/_web_users_clLogin.html') {
 				console.log("changing to front page...");
-				parent.document.location.href = 'http://www.kurt-climbing.com/Joomla';
+				parent.document.location.href = 'http://www.kurt-climbing.com';
 			}
 		}
 //	}
 	return origProcessAuthObj(urlAuthObj, successFunc);
 };
-
