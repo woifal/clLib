63e32319471dc0974f750554d33dc8bd1a53bc48
woifal <wolfgang.dietersdorfer@gmail.com>
2015-03-19 22:48:03 +0100

    Issue #186: Top10 stats...

--- js/_web/clLib.UI.web.GraphConfig.js
+++ js/_web/clLib.UI.web.GraphConfig.js
@@ -48,7 +48,7 @@ clLib.graphConfig = {
 			,className : this.graphName
             ,graphType: "bar"
             ,paging: true
-            ,pagingSteps: 200
+            ,pagingSteps: 10
             ,errorHandler : function(e) {
                 alert("encountered error >" + e + "<(" + JSON.stringify(e) + ")");
             }
@@ -56,7 +56,7 @@ clLib.graphConfig = {
                 //alert("getGraphData for options >" + JSON.stringify(options) + "<");
                 //var statsOptions = $.extend(this.config["statsOptions"], options);
                 options["statsOptions"] = this.config["statsOptions"];
-                console.error("stats options is >" + JSON.stringify(options) + "<");
+                console.log("stats options is >" + JSON.stringify(options) + "<");
                 return clLib.REST.requestStatsNew(
                     options
                     ,successFunc
@@ -75,69 +75,152 @@ clLib.graphConfig = {
                     options
                     ,function(resultObj) {
                         resultObj = JSON.parse(resultObj);
-                        console.error("success!!" + typeof(resultObj) + "-" + JSON.stringify(resultObj));
+                        console.log("success!!" + typeof(resultObj) + "-" + JSON.stringify(resultObj));
 
-                        var graphPoints = Object.keys(resultObj).sort();
-                        var graphLabels = graphPoints;
-                        //var graphLabels = clLib.formatArr(Object.keys(resultObj).sort(), [function(x) { return "" + x + "";}]);
-                        //graphLabels = graphLabels.sort();
-                        var graphData =  clLib.objToSortedArray(
-                            resultObj
-                            ,graphPoints
-                            ,function(x) { return parseInt(x["score"]); }
-                        );
-                        //var graphData = clLib.formatArrInt(clLib.getObjValues(resultObj, true), [function(x) { return parseInt(x.score); }]);
-                        graphData = graphData.slice(options["pagingStart"], graphConfig.pagingSteps);
-                        graphLabels = graphLabels.slice(options["pagingStart"], graphConfig.pagingSteps);
-                        util.log("graphData >" + JSON.stringify(graphData) + "<");
-                        util.log("graphLabels >" + JSON.stringify(graphLabels) + "<");
-                        var graphOptions = {
-                            animation: false
-                            ,bezierCurve: false
-                            ,datasetFill: false
-                        };
-                        var data = {
-                            labels : graphLabels
-                            ,datasets : [
-                                {
-                                    fillColor : "none"
-                                    ,strokeColor: "black"
-                                    ,data : graphData
+                        var graphTypes = graphConfig.graphType.split("|");
+                        
+                        if(
+                            graphTypes.indexOf("line") > -1 ||
+                            graphTypes.indexOf("bar") > -1 
+                        ) {
+                            var graphPoints = Object.keys(resultObj).sort();
+                            var graphLabels = graphPoints;
+                            //var graphLabels = clLib.formatArr(Object.keys(resultObj).sort(), [function(x) { return "" + x + "";}]);
+                            //graphLabels = graphLabels.sort();
+                            var graphData =  clLib.objToSortedArray(
+                                resultObj
+                                ,graphPoints
+                                ,function(x) { return parseInt(x["score"]); }
+                            );
+                            //var graphData = clLib.formatArrInt(clLib.getObjValues(resultObj, true), [function(x) { return parseInt(x.score); }]);
+                            graphData = graphData.slice(options["pagingStart"], graphConfig.pagingSteps);
+                            graphLabels = graphLabels.slice(options["pagingStart"], graphConfig.pagingSteps);
+                            util.log("graphData >" + JSON.stringify(graphData) + "<");
+                            util.log("graphLabels >" + JSON.stringify(graphLabels) + "<");
+                            var graphOptions = {
+                                animation: false
+                                ,bezierCurve: false
+                                ,datasetFill: false
+                                ,showTooltips: false
+                            };
+                            var data = {
+                                labels : graphLabels
+                                ,datasets : [
+                                    {
+                                        foo: 1
+                                        ,fillColor : "#EEEEEE"
+                                        ,strokeColor: "#EEEEEE"
+                                        ,highlightFill: "#FF0000"
+                                        ,data : graphData
+                                    }
+                                ]
+                            };
+
+                            //Get the context of the canvas element we want to select
+                            var ctx = $(graphConfig.collection.containerSelector).find(".clCanvas")[0].getContext("2d");
+                            if(window.myNewChart) {
+                                alert("clearing old chart!", "20150131");
+                                window.myNewChart.destroy();
+                            }
+                            else {
+                                clLib.loggi("no chart yet..>" + window.myNewChart + "<", "20150131");
+                            }
+
+                            if(graphTypes.indexOf("bar") > -1) {
+                                window.myNewChart = new Chart(ctx).Bar(data, graphOptions);
+                            } else if(graphTypes.indexOf("line") > -1 ) {
+                                window.myNewChart = new Chart(ctx).Line(data, graphOptions);
+                            }
+
+                            $(graphConfig.collection.containerSelector + " .clCanvas")[0].onclick = function(evt){
+                                console.log("chart clicked! >" + JSON.stringify(Object.keys(evt)) + "< + >" + evt.pageX + "<");
+                                var activePoints;
+                                if(graphTypes.indexOf("line") > -1) {
+                                    activePoints = window.myNewChart.getPointsAtEvent(evt);
                                 }
-                            ]
-                        };
+                                else if(graphTypes.indexOf("bar") > -1) {
+                                    activePoints = window.myNewChart.getBarsAtEvent(evt);
+                                }
+                                console.log("activePoints >" + JSON.stringify(activePoints) + "<");
+                                Chart.helpers.each(activePoints, function(activePoint){
+                                    window.myNewChart.eachBars(function(bar) {
+                                        console.log("Restoring bar >" + JSON.stringify(bar) + "<");
+                                        bar.restore(["fillColor"]);
+                                        bar.strokeColor = "#EEEEEE";
+                                        bar.fillColor = "#EEEEEE"; //rgba(220,220,220)";
+                                        //bar.highlightFill = "#EEEEEE";
+                                    });
 
-                        //Get the context of the canvas element we want to select
-                        var ctx = $(graphConfig.collection.containerSelector).find(".clCanvas")[0].getContext("2d");
-                        if(window.myNewChart) {
-                            clLib.loggi("clearing old chart!", "20150131");
-                            window.myNewChart.destroy();
-                        }
-                        else {
-                            clLib.loggi("no chart yet..>" + window.myNewChart + "<", "20150131");
-                        }
+                                    activePoint.strokeColor = "#063a72";
+                                    activePoint.fillColor = "#063a72";
+                                    //activePoint.highlightFill = "#00FF00";
+                                    window.myNewChart.update();
+                                    
+                                    console.log("activePoint >" + JSON.stringify(activePoint) + "<");
+                                    if(
+                                        graphTypes.indexOf("table") > -1 
+                                        || 1
+                                    ) {
+                                        var $tableContainer = $("#tableContainer");
+                                        var resultItems = resultObj[activePoint["label"]]["items"];
+                                        $tableContainer.empty();
+                                        clLib.UI.web.createTable(
+                                        $tableContainer
+                                        ,{
+                                            entity: "RouteLog"
+                                            ,items: {"RouteLog": resultItems}
+                                            ,where : null /*{"username": "foo6@gmail.com"}*/
+                                            ,readonly: true
+                                        }
+                                        ,function() {
+                                            console.log("table builtttttttttttt!");
+                                        }
+                                        ,function(e) {
+                                            alert("table build error >" + JSON.stringify(e) + "<");
+                                        }
+                                        );
+                                    }
 
-                        if(graphConfig.graphType == "bar") {
-                            window.myNewChart = new Chart(ctx).Bar(data, graphOptions);
-                        } else if(graphConfig.graphType == "line") {
-                            window.myNewChart = new Chart(ctx).Line(data, graphOptions);
-                        }
 
-                        $(graphConfig.collection.containerSelector)[0].onclick = function(evt){
-                            //alert("chart clicked!");
-                            var activePoints;
-                            if(graphConfig.graphType == "line") {
-                                activePoints = window.myNewChart.getPointsAtEvent(evt);
-                            }
-                            else if(graphConfig.graphType == "bar") {
-                                activePoints = window.myNewChart.getBarsAtEvent(evt);
-                            }
-                            $.each(activePoints, function(idx, data) {
-                                //alert("clicked point >" + data["label"] + "<");
+
+
+
+
+                                    
+                                });
+                                // => activePoints is an array of points on the canvas that are at the same position as the click event.
+                                //alert("activePoints >" + JSON.stringify(activePoints) + "<");
+                            };
+                        }
+                        if(
+                            graphTypes.indexOf("table") > -1
+                        ) {
+                            var $tableContainer = $("#tableContainer");
+                            var $filterContainer = $("#filterContainer");
+
+                            var resultKeys = Object.keys(resultObj).sort();
+                            var resultItems = [];
+                            $.each(resultKeys, function(idx, resultKey) {
+                                resultItems = resultItems.concat(resultObj[resultKey]["items"]);
                             });
-                            // => activePoints is an array of points on the canvas that are at the same position as the click event.
-                            //alert("activePoints >" + JSON.stringify(activePoints) + "<");
-                        };
+
+                            $tableContainer.empty();
+                            clLib.UI.web.createTable(
+                                $tableContainer
+                                ,{
+                                    entity: "RouteLog"
+                                    ,items: {"RouteLog": resultItems}
+                                    ,where : null /*{"username": "foo6@gmail.com"}*/
+                                    ,readonly: true
+                                }
+                                ,function() {
+                                    console.log("table builtttttttttttt!");
+                                }
+                                ,function(e) {
+                                    alert("table build error >" + JSON.stringify(e) + "<");
+                                }
+                            );
+                        }
                         
                     }
                     ,graphConfig.errorHandler
@@ -202,6 +285,22 @@ routeLogConfig.add(new GraphConfig({
         ,aggTopX:               10
         ,sortDescFlag:          true
         ,nrOfEligibleDays:      365
+    }
+}));
+routeLogConfig.add(new GraphConfig({
+    graphName : "top10AllTimes"
+    ,displayName: "Top 10(ever)"
+    ,graphType: "bar|table"
+    ,statsOptions: {
+        entity:                 "RouteLog"
+        ,datePortionFuncName :  null
+        ,aggFuncName:           "aggregateById"
+        ,sortByFuncName:        "sort_score"
+        ,aggTopX:               1
+        ,sortDescFlag:          true
+        ,nrOfEligibleDays:      365
+        ,startIdx:                 0
+        ,endIdx:                   10
     }
 }));
 
