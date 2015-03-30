6ccf813fbcb7c09fb6700215c4a5855753afb19e
woifal <wolfgang.dietersdorfer@gmail.com>
2015-03-31 00:09:50 +0200

    Issue #220: Buddy feature..

--- _env/.profile.sh
+++ _env/.profile.sh
@@ -5,6 +5,6 @@ export MONGO_PWD=blerl1la
 
 export CLLIB_HOME=/d/private/gits/clLib
 
-export PATH=${PATH}:${CLLIB_HOME}/_env
+export PATH=${PATH}:${CLLIB_HOME}/_env:/D/PRIVATE/ProgramFiles/mongodb/bin
 
 cd $CLLIB_HOME
