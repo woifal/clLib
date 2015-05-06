1035fa512224d903ed0b5d0165955d537dfbd369
woifal <wolfgang.dietersdorfer@gmail.com>
2015-05-07 00:35:03 +0200

    ISsue #221: syntax error..

--- js/_web/clLib.PAGES.web.js
+++ js/_web/clLib.PAGES.web.js
@@ -1,16 +1,21 @@
 "use strict";
 
-var origProcessAuthObj = clLib.PAGES.processAuthObj;
-alert("custom pages!");
-clLib.auth.processAuthObj = function(urlAuthObj, successFunc) {
-	alert("OVERRIDDEN PROCESSAUTHOBJ!!!!" + parent.document.location.href);
-//	if(!successFunc) {
-		successFunc = function() {
-			if(parent.document.location.href == 'http://www.kurt-climbing.com/index.php/cllib-users-cllogin') {
-				console.log("changing to front page...");
-				parent.document.location.href = 'http://www.kurt-climbing.com';
-			}
-		}
-//	}
-	return origProcessAuthObj(urlAuthObj, successFunc);
-};
+//(function(){
+
+    var origProcessAuthObj = clLib.auth.processAuthObj;
+    console.log("custom pages!");
+
+
+    clLib.auth.processAuthObj = function(urlAuthObj, successFunc, noLogin) {
+        console.log("!!!!!!!asdfasdfa sOVERRIDDEN PROCESSAUTHOBJ!!!!" + parent.document.location.href);
+    //	if(!successFunc) {
+            successFunc = function() {
+                if(parent.document.location.href == 'http://www.kurt-climbing.com/index.php/cllib-users-cllogin') {
+                    console.log("changing to front page...");
+                    parent.document.location.href = 'http://www.kurt-climbing.com';
+                }
+            }
+    //	}
+        return origProcessAuthObj(urlAuthObj, successFunc, noLogin);
+    };
+//})();
