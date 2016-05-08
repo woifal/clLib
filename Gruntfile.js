36e6cb2756c49412f53fdbaa35706cc61cf504bb
Wolfgang Dietersdorfer <Wolfgang Dietersdorfer>
2016-05-08 21:36:24 +0200

    Issue #255: dev env enhancements..

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,37 @@ module.exports = function(grunt) {
 grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 		includereplace: {
-				app_dev: {
+				app_local: {
 					options: {
 						flatten: true
 						,globals: {
 							NOWW: "xxxxx"
 							,"clLib.REST.baseURI" : "http://localhost:1983/db"
 							,"clLib.REST.clLibServerURI" : "http://localhost:1983"
+                            ,DEBUG_OUTPUT: "false"
+							,ENABLE_DEBUG_VISIBILITY: "visible" 
+						}
+					// Task-specific options go here.
+					},
+					files : [
+						{
+							src: ['*.html', "*.xml"],
+							dest: 'dist/tmp/app/'
+						}
+						,{
+							src: ['js/*'],
+							dest: 'dist/tmp/app/'
+						}
+					]
+
+				}
+				app_dev: {
+					options: {
+						flatten: true
+						,globals: {
+							NOWW: "xxxxx"
+							,"clLib.REST.baseURI" : "http://cllibserverdev.herokuapp.com/db"
+							,"clLib.REST.clLibServerURI" : "http://cllibserverdev.herokuapp.com"
                             ,DEBUG_OUTPUT: "false"
 							,ENABLE_DEBUG_VISIBILITY: "visible" 
 						}
@@ -73,7 +97,7 @@ grunt.initConfig({
 						}
 					]
 				}
-				,web_dev: {
+				,web_local: {
                     //expand: true,
 					options: {
 						flatten: true
@@ -81,6 +105,31 @@ grunt.initConfig({
 							NOWW: new Date().toISOString()
 							,"clLib.REST.baseURI" : "http://localhost:1983/db"
 							,"clLib.REST.clLibServerURI" : "http://localhost:1983"
+                            ,DEBUG_OUTPUT: "true"
+							,ENABLE_DEBUG_VISIBILITY: "visible" 
+							,WEB_IMG_FOLDER_URL: "http://localhost:8082/files/views/assets/image"
+						}
+					// Task-specific options go here.
+					},
+					files : [
+						{ 
+							src: ['html/_web/*'],
+							dest: 'dist/tmp/web/'
+						}
+						,{ 
+                            src: ['js/*'],
+							dest: 'dist/tmp/web/'
+						}
+					]
+				}
+				,web_dev: {
+                    //expand: true,
+					options: {
+						flatten: true
+						,globals: {
+							NOWW: new Date().toISOString()
+							,"clLib.REST.baseURI" : "http://cllibserverdev.herokuapp.com/db"
+							,"clLib.REST.clLibServerURI" : "http://cllibserverdev.herokuapp.com"
                             ,DEBUG_OUTPUT: "true"
 							,ENABLE_DEBUG_VISIBILITY: "visible" 
 							,WEB_IMG_FOLDER_URL: "http://localhost:8082/files/views/assets/image"
@@ -127,6 +176,36 @@ grunt.initConfig({
 					]
 
 				}
+				
+				,server_local: {
+					options: {
+						flatten: true
+						,globals: {
+							NOWW: "xxxxx"
+							,"clLib.REST.baseURI" : "http://localhost:1983/db"
+							,"clLib.REST.clLibServerURI" : "http://localhost:1983"
+                            ,DEBUG_OUTPUT: "false"
+							,mongodbUser :  "clAdmin"
+							,mongodbPwd  : "blerl1la"
+							,mongodbHost : "ds023520.mlab.com"
+							,mongodbPort : "23520"
+							,mongodbDBName: "climbinglog_dev"
+							}
+					// Task-specific options go here.
+					},
+					files : [
+						{ 
+                            src: ['server/*'],
+							dest: 'dist/tmp/server/'
+						}
+						,{
+							src: ['js/*'],
+							dest: 'dist/tmp/server/'
+						}
+
+					]
+
+				}				
 				,server_prod: {
 					options: {
 						flatten: true
@@ -158,6 +237,36 @@ grunt.initConfig({
          },
 		copy: {
 				app_dev: {
+					expand: true,
+					//flatten: true,
+					files: [
+						{ src: ["js/*"], dest:"dist/app/" }
+						,{ src: ["css/*","css/**/*"],  dest:"dist/app/" }
+						,{ src: ["files/*", "files/**/*"] , dest:"dist/app/" }
+						,{ 
+							flatten: true
+							,expand: true
+							,cwd: "dist/tmp/app"
+							,src: [
+								"**"
+							]
+							,dest:"dist/app" 
+						}
+						,
+						{ 
+							flatten: true
+							,expand: true
+							,cwd: "dist/tmp/app/js"
+							,src: [
+								"**"
+							]
+							,dest:"dist/app/js" 
+						}
+						,
+						
+					]
+				}
+				app_local: {
 					expand: true,
 					//flatten: true,
 					files: [
@@ -378,6 +487,41 @@ grunt.initConfig({
                         }
                      ]
 				}
+				,server_local: {
+					expand: true,
+					files: [
+						{ 
+							flatten: true
+							,expand: true
+							,cwd: "dist/tmp/server/server"
+							,src: [
+								"*"
+							]
+							,dest:"dist/server" 
+						}
+						,
+						{ 
+							flatten: true
+                            ,expand: true
+							,src: [
+								"dist/tmp/server/js/clLib.js", "dist/tmp/server/js/clLib.gradeConfig.js", "!dist/tmp/server/js/_web"
+							]
+							,dest:"dist/server" 
+                        }
+						,
+					{ 
+                          expand: true
+                          ,src: ["server/node_modules/*", "server/node_modules/**/*"] 
+                          ,dest:"dist" 
+                      }
+                        ,
+                        { 
+                            expand: true
+                            ,src: ["server/appCertificates/*", "server/appCertificates/**/*"] 
+                            ,dest:"dist" 
+                        }
+                     ]
+				}				
 		},
         compress: {
 			  app_dev: {
@@ -398,6 +542,24 @@ grunt.initConfig({
 				  }, // includes files in path
 				]
 			  }
+			  app_local: {
+				options: {
+				  archive: 'dist/app/archive.zip'
+				},
+				files: [
+				  {
+				  expand: true,
+				  src: [
+					//1
+					'**'
+					,
+					'!archive.zip',
+				  ]
+				  , dest: '.'
+				  , cwd: 'dist/app/'
+				  }, // includes files in path
+				]
+			  }			  
 			  ,app_prod: {
 				options: {
 				  archive: 'dist/app/archive.zip'
@@ -425,10 +587,13 @@ grunt.initConfig({
     
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('appdev', ['includereplace:app_dev', 'copy:app_dev']);
+    grunt.registerTask('applocal', ['includereplace:app_local', 'copy:app_local']);
     grunt.registerTask('appprod', ['includereplace:app_prod', 'copy:app_prod', 'compress:app_prod']);
     grunt.registerTask('webprod', ['includereplace:web_prod', 'copy:web_prod']);
     grunt.registerTask('webdev', ['includereplace:web_dev', 'copy:web_prod']);
+    grunt.registerTask('weblocal', ['includereplace:web_local', 'copy:web_local']);
     grunt.registerTask('serverprod', ['includereplace:server_prod', 'copy:server_prod']);
     grunt.registerTask('serverdev', ['includereplace:server_dev', 'copy:server_dev']);
+    grunt.registerTask('serverlocal', ['includereplace:server_local', 'copy:server_local']);
 
 };
